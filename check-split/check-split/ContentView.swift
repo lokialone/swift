062c06aedfe29fc537afd2f6cc14963fc8a542cf
loki <648875862@qq.com>
2020-01-12 22:34:14 +0800

    feat: init check-split

---
+++ check-split/check-split/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  check-split
+//
+//  Created by loki on 2020/1/12.
+//  Copyright © 2020 loki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
